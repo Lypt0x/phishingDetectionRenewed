57e4b9b7a83a6a28871e144a705605482d82dbf4
Lypt0x <2xlyptox@gmail.com>
2021-11-21 14:51:27 +0100

    Explanation about to start

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,15 @@ use twilight_command_parser::CommandParserConfig;
 use crate::rest::Safe;
 use anyhow::Result;
 
+/*
+ * This is the main function of the bot.
+ * It is called when the bot is started.
+ * 
+ * In order to start the bot you need to set the following environment variables:
+ *  export DISCORD_TOKEN=your_token_here
+ *  export APP=your_app_id_here
+ */
+
 #[tokio::main]
 async fn main() -> Result<()> {
     let safe = Safe::new("db").await?;
